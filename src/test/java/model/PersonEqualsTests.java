e8db5a6160ae455b664d3e4f3dcdae0ad818ffa7
Tan Kupreeva <tanya.bukrei@mail.ru>
2023-07-05 20:13:05 +0300

    initial commit

---
+++ src/test/java/model/PersonEqualsTests.java
@@ -0,0 +1,38 @@
+package model;
+
+public class PersonEqualsTests {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("John");
+        p.setLastName("Johnson");
+        p.setAge(55);
+        p.setId(100);
+
+        Person p2 = new Person(100, "John", "Johnson", 55);
+        //== ссылки всегда различны для разных объектов!!!!!
+        System.out.println("p == p2 ? " + (p == p2));
+        Person p100 = p;
+        System.out.println("p100 == p ? " + (p100 == p));
+
+        // equals по умолчанию проверяет ссылки
+        // если непереопределить equals всегда будут сравниваться ссылки
+        Cat cat1 = new Cat("Murzik");
+        Cat cat2 = new Cat("Murzik");
+
+
+        Person person1 = new Person(123, "Mike", "Tyson", 56);
+        person1.setCat(cat1);
+        Person person2 = new Person(123, "Mike", "Tyson", 56);
+        person2.setCat(cat2);
+        //для того чтобы объекты сравнивались коррекно необх-мо переопределить метод equals/как правило объекты сравниваются по полям
+        System.out.println("person1 equals person2 " + person1.equals(person2));
+
+        // hashcode ф к-ая вернет целое число
+        System.out.println("cat1 hashcode : " + cat1.hashCode());
+        System.out.println("cat2 hashcode : " + cat2.hashCode());
+        System.out.println("person 1 hashcode : " + person1.hashCode());
+        System.out.println("person 2 hashcode : " + person2.hashCode());
+
+
+    }
+}
