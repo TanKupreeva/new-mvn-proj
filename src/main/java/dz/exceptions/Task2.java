92dcf7d28f456fe9c34dc38c0f352bf9b34d7b83
TanKupreeva <tanya.bukrei@mail.ru>
2023-07-17 18:06:53 +0300

    Exceptions

--- src/main/java/dz/exceptions/Task2.java
+++ src/main/java/dz/exceptions/Task2.java
@@ -4,19 +4,31 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[] m = {-1, 0, 1};
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        try {
-            m[a] = 4 / a;
-            System.out.println(m[a]);
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на ноль!");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Please user next indexes bound -> from 1 to " + (m.length - 1));
-        } catch (Exception e) {
-            System.out.println("Input must be an integer");
-        }
+        while (true) {
+            int[] m = {-1, 0, 1};
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            try {
+                m[a] = 4 / a;
+                System.out.println(m[a]);
+            } catch (ArithmeticException e) {
+                System.out.println("Деление на ноль!");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Please user next indexes bound -> from 1 to " + (m.length - 1));
+            } catch (Exception e) {
+                System.out.println("Input must be an integer");
+            }
+            char o;
+            do {
+                System.out.println("Желаете продолжить? y/n");
+                o = sc.next().charAt(0);
 
+            }
+            while (o != 'n' && o != 'y');
+
+            if (o == 'n') {
+                break;
+            }
+        }
     }
 }
