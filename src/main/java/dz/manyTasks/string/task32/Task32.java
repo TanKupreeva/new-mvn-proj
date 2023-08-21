ce33074e084422548fa51afc2ed6caf48a5b4e66
Tan Kupreeva <tanya.bukrei@mail.ru>
2023-08-21 18:57:25 +0300

    polindrom

--- src/main/java/dz/manyTasks/string/task32/Task32.java
+++ src/main/java/dz/manyTasks/string/task32/Task32.java
@@ -11,20 +11,16 @@ public class Task32 {
         char[] b = str2.toCharArray();
         StringBuffer polindrom = new StringBuffer();
         String otvet = "";
-        String ot = "";
+
 
         for (int i = 0, k = 0; i < str.length(); k++) {
             for (int j = 0; j < sb.length(); j++) {
                 if (a[i] == b[j]) {
                     if (i < a.length - 1) {
                         polindrom = polindrom.append(a[i]);
-                        ot=polindrom.toString();
-                        System.out.println(polindrom.length());
-                        System.out.println(polindrom);
-                        System.out.println(otvet.length());
-                        if (otvet.length() < ot.length()) {
-                            otvet = ot;
-                            System.out.println(otvet);
+
+                        if (otvet.length() <= polindrom.length()) {
+                            otvet = polindrom.toString();
                         }
                         i++;
                     }
@@ -33,12 +29,12 @@ public class Task32 {
                     i = k;
                 }
             }
-            if (k == str.length() - 1) {
-                return;
+            if (k == str.length()-1) {
+                break;
             }
         }
 
-        System.out.println(otvet);
+        System.out.println("Polindrom: " + otvet + "  length = " + otvet.length());
 
 
     }
